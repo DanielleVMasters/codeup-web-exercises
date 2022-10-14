e9984a28db4e50fa1f2647c790f6eb961560ff45
DanielleVMasters <danielle.victoria.masters@gmail.com>
2022-10-14 14:25:04 -0500

    Do While set up but not finished

--- js/while.js
+++ js/while.js
@@ -6,3 +6,13 @@ while (i <= 65536) {
     console.log(i);
     i += i;
 }
+
+let allCones = Math.floor(Math.random() * 50) + 50; //start point
+let x = Math.floor(Math.random() * 5) + 1;
+let cl = allCones - x
+do {
+    console.log(x + " cones sold.");
+    console.log("Cannot sell you " + x + " I only have " + cl);
+    console.log("Yay! I sold them all");//output
+    cl--;//up by
+} while (allCones < x);//how many
