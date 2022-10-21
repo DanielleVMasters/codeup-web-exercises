da962043460ec91c3c531eecb59475d9767811c1
DanielleVMasters <danielle.victoria.masters@gmail.com>
2022-10-21 09:31:15 -0500

    morning warmup

--- js/warmups.js
+++ js/warmups.js
@@ -1,9 +1,23 @@
-function GetPrice(object){
-    return object.price;
+//10/20/22
+// function GetPrice(object){
+//     return object.price;
+// }
+//
+// let potatoes = {
+//     name: "Potatoes",
+//     quantity: 2,
+//     price: "$7.89"
+// }
+
+//10/21/22
+function getLString(array){
+    let Lstring = ""
+    array.forEach(function(string){
+        if(string.length > Lstring.length) {
+            Lstring = string
+        }
+    })
+    return Lstring
 }
 
-let potatoes = {
-    name: "Potatoes",
-    quantity: 2,
-    price: "$7.89"
-}
+console.log(getLString(["blue", "green", "Periwinkle"]))
