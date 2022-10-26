3cb2be2607225b62e503768df87454871a802b35
DanielleVMasters <danielle.victoria.masters@gmail.com>
2022-10-26 10:12:38 -0500

    morning warmup and part 1 of the box model assignment

--- js/warmups.js
+++ js/warmups.js
@@ -21,3 +21,51 @@ function getLString(array){
 }
 
 console.log(getLString(["blue", "green", "Periwinkle"]))
+
+//10/26/22
+const hamsters = [
+    {
+        name: "Hamtaro",
+        heightInMM: 86,
+        fur: ['orange', 'white'],
+        gender: "male",
+        dateOfBirth: "August 6"
+    } , {
+        name: "Bijou",
+        heightInMM: 75,
+        fur: ['white'],
+        gender: "female",
+        dateOfBirth: "July 10"
+    } , {
+        name: "Oxnard",
+        heightInMM: 100,
+        fur: ['grey', 'white'],
+        gender: "male",
+        dateOfBirth: "May 3"
+    } , {
+        name: "Boss",
+        heightInMM: 120,
+        fur: ['brown', 'white'],
+        gender: "male",
+        dateOfBirth: "September 21"
+    } , {
+        name: "Snoozer",
+        heightInMM: 85,
+        fur: ['brown', 'white', "pink"],
+        gender: "male",
+        dateOfBirth: "January 14"
+    }
+];
+
+// should return {name: "Boss", heightInMM: 120, fur: ['brown', 'white'], gender: "male", dateOfBirth: "September 21"};
+function getTallestHamster(array){
+    let tallest = {heightInMM: 0};
+    array.forEach(function(ham){
+        if (ham.heightInMM > tallest.heightInMM) {
+            tallest = ham;
+        }
+    })
+    return tallest;
+
+}
+console.log(getTallestHamster(hamsters))
