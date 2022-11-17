2c65436f6f5ad7208946d5a92de2ed3d401998f8
DanielleVMasters <danielle.victoria.masters@gmail.com>
2022-11-17 15:14:07 -0600

    Element selector section done

--- js/JQuary.js
+++ js/JQuary.js
@@ -1,5 +1,9 @@
 $(function() {
     // let contentalert = $("#title").html();
     // alert(contentalert)
-    $(".codeup").css("border", "1px solid red")
+    // $(".codeup").css("border", "1px solid red")
+    $("li").css("font-size", "20px")
+    $("h1, p, li").css("background-color", "yellow")
+    let elementalert = $("h1").html()
+    alert(elementalert)
 });
