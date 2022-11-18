fa1ee988a20f989113365d60c659c33e125434c6
DanielleVMasters <danielle.victoria.masters@gmail.com>
2022-11-18 10:40:44 -0600

    mouse event assignment complete

--- js/JQuary.js
+++ js/JQuary.js
@@ -1,9 +1,27 @@
 $(function() {
-    // let contentalert = $("#title").html();
-    // alert(contentalert)
-    // $(".codeup").css("border", "1px solid red")
-    $("li").css("font-size", "20px")
-    $("h1, p, li").css("background-color", "yellow")
-    let elementalert = $("h1").html()
-    alert(elementalert)
+    // // let contentalert = $("#title").html();
+    // // alert(contentalert)
+    // // $(".codeup").css("border", "1px solid red")
+    // $("li").css("font-size", "20px")
+    // $("h1, p, li").css("background-color", "yellow")
+    // let elementalert = $("h1").html()
+    // alert(elementalert)
+    $("h1").click(
+        function(){
+            $(this).css("background", "peachpuff");
+        }
+    )
+    $("p").dblclick(
+        function(){
+            $(this).css("font-size", "18px");
+        }
+    )
+    $("li").hover(
+        function() {
+            $(this).css("color", "red");
+        },
+        function() {
+            $(this).css("color", "black");
+        }
+    )
 });
