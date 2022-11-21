1a648c1a9c28dee39e61398aa89f3f11796906b0
DanielleVMasters <danielle.victoria.masters@gmail.com>
2022-11-21 10:38:35 -0600

    assignment complete

---
+++ js/jquery.js
@@ -0,0 +1,37 @@
+let CheatKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down',
+    65: 'a',
+    66: 'b',
+    13: 'enter'
+};
+let konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a', 'enter'];
+let konamiCodePosition = 0;
+
+$(function(){
+    $(document).keydown(function(e){
+        let key = CheatKeys[e.keyCode];
+        let requiredKey = konamiCode[konamiCodePosition];
+        if (key == requiredKey) {
+            konamiCodePosition++;
+            if (konamiCodePosition == konamiCode.length) {
+                activateCheats();
+                konamiCodePosition = 0;
+            }
+        } else {
+            konamiCodePosition = 0;
+        }
+    })
+    function activateCheats() {
+        document.body.style.backgroundImage = "url('pictures/MedicalPeacefulGalapagossealion-size_restricted.gif')";
+        let audio = new Audio('audio/Konami.mp3');
+        audio.play();
+        $('h1').css("color", "white")
+    }
+
+});
+
+
+
