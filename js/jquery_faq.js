ff434c82f0ea8a74374ccf010ce62dc1f5104840
DanielleVMasters <danielle.victoria.masters@gmail.com>
2022-11-28 11:09:24 -0600

    update assignment

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -7,30 +7,16 @@ $(function() {
     })
 
     $("#btn-2").click(function(){
-        $("ul").children(".4").css("background", "yellow")
+        $("ul").each(function(){
+            $(this).children().last().css("background", "yellow")
+        })
     })
 
-    $("h3.h31").click(function(){
-        $("ul.ul1").children().css("font-weight", "bold")
+    $("h3").click(function(){
+        $(this).next().children().css("font-weight", "bold")
     })
 
-    $("h3.h32").click(function(){
-        $("ul.ul2").children().css("font-weight", "bold")
-    })
-
-    $("h3.h33").click(function(){
-        $("ul.ul3").children().css("font-weight", "bold")
-    })
-
-    $("ul.ul1").click(function(){
-        $("ul.ul1").children(".1").css("color", "blue")
-    })
-
-    $("ul.ul2").click(function(){
-        $("ul.ul2").children(".1").css("color", "blue")
-    })
-
-    $("ul.ul3").click(function(){
-        $("ul.ul3").children(".1").css("color", "blue")
+    $("li").click(function(){
+        $(this).parent().children().first().css("color", "blue")
     })
 });
