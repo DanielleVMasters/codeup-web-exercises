c68ecfa2e595670d496e280c36cf49b60216ef6d
DanielleVMasters <danielle.victoria.masters@gmail.com>
2022-11-28 09:56:39 -0600

    completed traversing assignment

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -2,7 +2,35 @@ $(function() {
     $("dd")
         .addClass("invisible")
 
-    $("#btn-2").click(function(){
+    $("#btn-1").click(function(){
         $("dd").toggleClass("invisible")
+    })
+
+    $("#btn-2").click(function(){
+        $("ul").children(".4").css("background", "yellow")
+    })
+
+    $("h3.h31").click(function(){
+        $("ul.ul1").children().css("font-weight", "bold")
+    })
+
+    $("h3.h32").click(function(){
+        $("ul.ul2").children().css("font-weight", "bold")
+    })
+
+    $("h3.h33").click(function(){
+        $("ul.ul3").children().css("font-weight", "bold")
+    })
+
+    $("ul.ul1").click(function(){
+        $("ul.ul1").children(".1").css("color", "blue")
+    })
+
+    $("ul.ul2").click(function(){
+        $("ul.ul2").children(".1").css("color", "blue")
+    })
+
+    $("ul.ul3").click(function(){
+        $("ul.ul3").children(".1").css("color", "blue")
     })
 });
